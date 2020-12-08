30064d098edf3bad4cc65d646b88436fa955b283
sudeshkunwar@outlook.com <sudeshkunwar@outlook.com>
2020-12-08 21:27:20 +0545

    initial

---
+++ app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -0,0 +1,28 @@
+package com.example.twoactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+    public static final String EXTRA_REPLY =
+            "com.example.android.twoactivities.extra.REPLY";
+    private EditText mReply;
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
+
+
+
+    public void returnReply(View view) {
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        Log.d(LOG_TAG, "End SecondActivity");
+        finish();
+    }
+}
